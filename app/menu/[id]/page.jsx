e8c0bafa56b794824f373fd4c6240946f3864838
lichpark <lichpark0@hanmail.net>
2024-12-02 17:00:25 +0900

    page data use

--- app/menu/[id]/page.jsx
+++ app/menu/[id]/page.jsx
@@ -1,6 +1,20 @@
-const Page = ({ params }) => {
+import Card from "@/app/components/Card";
+
+const Page = async ({ params }) => {
+  const data = await fetch("https://api.sampleapis.com/coffee/hot").then(
+    (response) => response.json()
+  );
+
   const { id } = params;
-  return <div>id: {id} </div>;
+  return (
+    <div style={{ width: "100px", height: "100px", margin: "10% auto" }}>
+      <Card
+        img={data[id].image}
+        itemName={data[id].title}
+        altText={data[id].title}
+      />
+    </div>
+  );
 };
 
 export default Page;
