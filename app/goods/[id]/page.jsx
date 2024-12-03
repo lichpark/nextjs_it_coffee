27e5419226537e370a0145514bd5e35148144111
lichpark <lichpark0@hanmail.net>
2024-12-03 17:36:27 +0900

    json data page next.js

--- app/goods/[id]/page.jsx
+++ app/goods/[id]/page.jsx
@@ -1,7 +1,37 @@
-const Page = ({ params }) => {
-  const { id } = params;
+const Page = async ({ params }) => {
+  const { id } = await params;
 
-  return <div>굿즈 안에 아이디! {id} </div>;
+  const goodsData = await fetch(
+    `https://dummyjson.com/products/search?q=${id}`
+  ).then((res) => res.json());
+  console.log(goodsData);
+  return (
+    <div
+      style={{
+        width: "500px",
+        height: "500px",
+        margin: "5% auto",
+      }}
+    >
+      {/* 굿즈 안에 아이디! {id} */}
+
+      <div>
+        <article>
+          <img
+            // src={goodsData.products[id - 1].images[0]}
+            src={goodsData.products[0].images[0]}
+            style={{ width: "100%" }}
+          />
+        </article>
+        <div>
+          <span>
+            {/* {goodsData.products[id - 1].title} */}
+            {goodsData.products[0].title}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
